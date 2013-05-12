c074ab9808ef4287d169cce286ca76175886d618
Kevin Chang <kevinjhc@gmail.com>
2013-05-12 18:47:00 -0400

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Demoday::Application.routes.draw do
   
+  root :to => 'static_pages#index'
+
   match '/map', to: 'static_pages#map'
 
   resources :startups
   match '/follow', to: 'startups#follow'
   match '/send_email', to: 'startups#send_email'
 
+  # Angellist Oauth
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'signout', to: 'sessions#destroy', as: 'signout'
-
-  root :to => 'static_pages#index'
 
 end
