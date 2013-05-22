facd5482181f0768133893974af19296b82948a1
Kevin Chang <kevinjhc@gmail.com>
2013-05-22 15:14:43 -0400

    no more paperclip

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,9 +1,8 @@
 class Startup < ActiveRecord::Base
-  attr_accessible :angellist_id, :bio, :ceo_name, :email, :name, :presentation_order, :ceo_angellist_id, :domain_url, :ceo_slug, :avatar, :avatar_file_name
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  attr_accessible :angellist_id, :bio, :ceo_name, :email, :name, :presentation_order, :ceo_angellist_id, :domain_url, :ceo_slug
 
-  # def img_url
-  #   self.name.downcase + '.jpg'
-  # end
+  def img_url
+    self.name.downcase + '.jpg'
+  end
   
 end
