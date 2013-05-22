c5893917b5150856e1c7aee7c4a830b436a1cc97
Kevin Chang <kevinjhc@gmail.com>
2013-05-22 14:50:07 -0400

    added accessor for image url

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,5 +1,5 @@
 class Startup < ActiveRecord::Base
-  attr_accessible :angellist_id, :bio, :ceo_name, :email, :name, :presentation_order, :ceo_angellist_id, :domain_url, :ceo_slug, :avatar
+  attr_accessible :angellist_id, :bio, :ceo_name, :email, :name, :presentation_order, :ceo_angellist_id, :domain_url, :ceo_slug, :avatar, :avatar_file_name
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
   # def img_url
